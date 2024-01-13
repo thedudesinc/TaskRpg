a5a0660b39e34feaf67dfc9b298fb90ca5824edf
Zachary Pruessner <zachpruessner@gmail.com>
2024-01-13 15:55:53 -0600

    installed tailwind and flowbite

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import 'flowbite';
+import type { ModalOptions, ModalInterface } from 'flowbite';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
